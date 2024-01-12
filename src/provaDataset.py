f6e505b01dade4396fe04226162a1e1efd63cdf5
Raffaele Vietri <RaffaeleVietriASRoma@gmail.com>
2024-01-12 16:21:56 +0100

    fix(backend): fixo problemi di compatibilità con windows

--- src/provaDataset.py
+++ src/provaDataset.py
@@ -1,13 +1,13 @@
 from src.preProcessing import *
 
-# pathdataset = "Carburanti.csv"
-# dfLetto = letturaDataset(pathdataset)
-# dataCleaning(dfLetto,"NOME_PRODOTTO")
+pathdataset = "Carburanti.csv"
+dfLetto = letturaDataset(pathdataset)
+dataCleaning(dfLetto,"NOME_PRODOTTO")
 
 pathdataset = "Titanic-Dataset.csv"
 dfLetto = letturaDataset(pathdataset)
 dataCleaning(dfLetto,"Survived")
 
-# pathdataset = "myntra_products_catalog.csv"
-# dfLetto = letturaDataset(pathdataset)
-# dataCleaning(dfLetto,"Gender")
+pathdataset = "myntra_products_catalog.csv"
+dfLetto = letturaDataset(pathdataset)
+dataCleaning(dfLetto,"Gender")
